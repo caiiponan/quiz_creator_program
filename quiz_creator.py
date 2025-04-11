d5562a261d2a8623959751a6e07103b703621d55
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-04-11 17:10:42 +0800

    Created add question and exit button. Their respective commands will be added later in this code.

--- quiz_creator.py
+++ quiz_creator.py
@@ -53,9 +53,18 @@ class QuizCreatorGUI:
         self.correct_answer_label.pack(padx = 5, pady = 5)
         self.correct_answer_entry = ttk.Entry(correct_answer_frame, width = 10)
         self.correct_answer_entry.pack(padx = 5, pady = 5)
-        
+
 # Create Frame to hold buttons
+        buttons_frame = ttk.Frame(self.root)
+        buttons_frame.pack(padx = 10, pady = 10, fill = tk.X)
+
 # Create 'Add Question' and 'Exit' buttons with associated commands
+        self.add_button = ttk.Button(buttons_frame, text = "Add Question", command = self.add_question)
+        self.add_button.pack(side = tk.LEFT, padx = 5)
+
+        self.exit_button = ttk.Button(buttons_frame, text = "Exit", command = self.exit_program)
+        self.exit_button.pack(side = tk.RIGHT, padx = 5)
+        
 # Create a ScrolledText widget to display status messages
 # Arrange all the created widgets using the pack and grid layout managers
 # Define clear_entries to clear all input fields
