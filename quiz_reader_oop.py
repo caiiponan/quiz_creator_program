33dda3dac181b0ad281c1583194f2984b3ad585b
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-05-30 01:16:53 +0800

    Error checking. This block checks if there will be any errors that will occur such as that if the file is empty or is not found.

--- quiz_reader_oop.py
+++ quiz_reader_oop.py
@@ -4,3 +4,14 @@ class QuizReader:
     def __init__(self, file_name):
         self.file_name = file_name
         self.question_list = self.load_questions()
+    
+    def load_questions(self):
+        try:
+            with open(self.file_name, 'r') as file:
+                lines = file.readlines()
+                if not lines:
+                    print("The file is empty.")
+                    return []
+        except FileNotFoundError:
+            print(f"Error: The file '{self.file_name}' was not found.")
+            return []
